8355c9910b65d1bf4f18f1cdc9cfefe487cc3e6e
Brandon Wu <bwu14500@usc.edu>
2023-05-31 04:14:13 -0700

    new commit

--- solution.cpp
+++ solution.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    return 0;
+vector<int> twoSum(vector<int>& nums, int target) {
+    for (int i = 0; i < nums.size(); i++) {
+        for (int j = i+1; j < nums.size(); j++) {
+            if (nums[i]+nums[j] == target) {
+                return {i,j};
+            }
+        }
+    }
+    return {};
 }
